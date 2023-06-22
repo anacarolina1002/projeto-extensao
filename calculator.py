a9767aab67dba17a66c2d5d88b021e661e3e8bdc
Ana Carolina Machado <machado.mcd.ana@gmail.com>
2023-06-21 21:43:05 -0300

    :rainbow: New Commit

--- calculator.py
+++ calculator.py
@@ -7,21 +7,22 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def calculator():
     if request.method == 'POST':
-        weight = float(request.form.get('weight', 0))
+        name = request.form.get('name', '')  # Obter o valor do nome
+        weight = request.form.get('weight', 0)  # Obter o valor do peso sem conversão
+        weight = float(weight.replace(',', '.'))  # Substituir a vírgula por ponto e converter para float
         height = float(request.form.get('height', 0))
         age = float(request.form.get('age', 0))
         sex = request.form.get('sex', '')
 
-        if sex == 'female':
+        if sex == 'feminino':
             result = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
-        elif sex == 'male':
+        elif sex == 'masculino':
             result = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
-        else:
-            result = 'Sexo inválido'
 
         # Usando a biblioteca pandas para manipular os dados se necessário
-        df = pd.DataFrame({'Peso': [weight], 'Altura': [height], 'Idade': [age], 'Sexo': [sex], 'TMB': [result]})
-        df.to_csv('calculations.csv', mode='a', header=not os.path.exists('calculations.csv'))
+        df = pd.DataFrame({'Nome': [name], 'Peso': [weight], 'Altura': [height], 'Idade': [age], 'Sexo': [sex], 'TMB': [result]})
+        df['Peso'] = df['Peso'].apply(lambda x: int(x) if x.is_integer() else x)  # Converter para int se for um número inteiro
+        df.to_csv('calculations.csv', mode='a', header=not os.path.exists('calculations.csv'), index=False, float_format='%.2f')
 
         return render_template('index.html', result=result)
     else:
